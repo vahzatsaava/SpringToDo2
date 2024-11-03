ba4ec68f2a9aef2d96e44b3860a31b738b7efe8d
Radion Vygonyailo <coll-radion@yandex.ru>
2024-11-03 19:07:38 +0300

    Init commit

---
+++ src/main/java/com/emobile/springtodo/SpringToDoApplication.java
@@ -0,0 +1,13 @@
+package com.emobile.springtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringToDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringToDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emobile/springtodo/SpringToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emobile.springtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
